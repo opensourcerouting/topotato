83b762eaacfa5c4a8aedcb418645ff54101e6d55
Rodrigo Nardi <rnardi@netdef.org>
2025-10-06 12:48:02 -0300

    feat: integrate react-virtuoso for virtualized rendering in LogTable component

--- topotato/react-tomato/src/component/LogTable.jsx
+++ topotato/react-tomato/src/component/LogTable.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Virtuoso } from 'react-virtuoso';
 import './LogTable.css';
 
 function getRowClass(level) {
@@ -16,39 +17,32 @@ function getRowClass(level) {
   }
 }
 
-function formatMessage(message) {
-  if (!message) return null;
-
-  let formatted = message.replace(/(https?:\/\/\S+)/g, '<a href="$1" target="_blank" rel="noopener noreferrer">$1</a>');
-
-  formatted = formatted.replace(/\*\*(.*?)\*\*/g, '<b>$1</b>');
-
-  formatted = formatted.replace(/_(.*?)_/g, '<i>$1</i>');
-
-  formatted = formatted.replace(/\[(.*?)\]/g, '<span class="log-file">[$1]</span>');
-  return <span dangerouslySetInnerHTML={{ __html: formatted }} />;
-}
-
 const LogTable = ({ logs, timed }) => (
   <div className="log-table-container">
     <div className="log-status-row log-status-success">
       <span>&#9654; passed after {timed || '0.00s'}</span>
     </div>
-    <table className="log-table">
-      <tbody>
-        {logs.map((log, idx) => (
-          <tr key={idx} className={getRowClass(log.data.type)}>
+    <div style={{ height: 400, width: '100%' }}>
+      <Virtuoso
+        data={logs}
+        style={{ height: 360, width: '100%' }}
+        components={{
+          List: React.forwardRef((props, ref) => (
+            <table className="log-table"><tbody ref={ref} {...props} /></table>
+          )),
+        }}
+        itemContent={(idx, log) => (
+          <tr className={getRowClass(log.data.type)}>
             <td className="log-time">{log.ts}</td>
             <td className="log-router">{log.data.router}</td>
             <td className="log-daemon">{log.data.daemon}</td>
             <td className="log-text">{log.data.text}</td>
             <td className="log-level">{log.data.type}</td>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        )}
+      />
+    </div>
   </div>
 );
 
 export default LogTable;
-
